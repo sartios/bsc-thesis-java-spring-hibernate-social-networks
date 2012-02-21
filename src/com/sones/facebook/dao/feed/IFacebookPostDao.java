2d20df948694cb1bdcdc6c856ddef2e2f86739d3
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-02-21 21:12:34 +0000

    Checkin whole project

---
+++ src/com/sones/facebook/dao/feed/IFacebookPostDao.java
@@ -0,0 +1,9 @@
+package com.sones.facebook.dao.feed;
+
+import com.sones.dao.IGenericDao;
+import com.sones.facebook.model.feed.FacebookPost;
+
+public interface IFacebookPostDao extends IGenericDao<FacebookPost, String>
+{
+
+}
