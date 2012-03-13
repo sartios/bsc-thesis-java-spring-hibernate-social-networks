d145591f729ebc948aef8482344ec24e46901744
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-13 20:03:45 +0000

    Clear com.sones.dao.feed

--- src/com/sones/dao/feed/IEventPostDao.java
+++
@@ -1,9 +0,0 @@
-package com.sones.facebook.dao.feed;
-
-import com.sones.dao.IGenericDao;
-import com.sones.facebook.model.feed.EventPost;
-
-public interface IEventPostDao extends IGenericDao<EventPost, String>
-{
-
-}
--- src/com/sones/dao/feed/INoteDao.java
+++
@@ -1,9 +0,0 @@
-package com.sones.facebook.dao.feed;
-
-import com.sones.dao.IGenericDao;
-import com.sones.facebook.model.feed.Note;
-
-public interface INoteDao extends IGenericDao<Note, String>
-{
-
-}
