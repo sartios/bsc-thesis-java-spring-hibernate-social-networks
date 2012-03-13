540ebaf0bd5b000c49ff699b3da6993b09990e63
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-13 19:54:38 +0000

    Add Dao Interfaces for facebook downloader model.

---
+++ trunk/test/com/sones/facebook/dao/source/IPageDao.java
@@ -0,0 +1,9 @@
+package com.sones.facebook.dao.source;
+
+import com.sones.dao.IGenericDao;
+import com.sones.facebook.model.source.Page;
+
+public interface IPageDao extends IGenericDao<Page, String>
+{
+
+}
