f4f5ceff5622c18abd8745f137eeeb3d23c97fdb
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-22 09:14:12 +0000

    Add mising dao interfaces for facebook downloader.

---
+++ src/com/sones/facebook/dao/source/ISourceDao.java
@@ -0,0 +1,14 @@
+package com.sones.facebook.dao.source;
+
+import com.sones.dao.IGenericDao;
+import com.sones.facebook.model.source.Source;
+
+/**
+ * Provides methods for accessing {@link Source} class.
+ * @author sartios.sones@gmail.com.
+ *
+ */
+public interface ISourceDao extends IGenericDao<Source, String>
+{
+
+}
