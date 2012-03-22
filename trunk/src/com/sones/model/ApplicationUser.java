51c8e14c8710884a079aed877e1078689cfd3196
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-22 09:17:44 +0000

    Update Application User model

--- trunk/src/com/sones/model/ApplicationUser.java
+++ trunk/src/com/sones/model/ApplicationUser.java
@@ -2,6 +2,13 @@ package com.sones.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sones.dao.DatabaseConstants;
+
 /**
  * <b>Table:</b> FCBK.APPLICATION_USERS <br/><br/>
  * Represents a user of the application.
@@ -9,12 +16,16 @@ import java.io.Serializable;
  * @author sartios.sones@gmail.com.
  *
  */
+@Entity
+@Table(name=ApplicationUserConstants.TABLE_NAME,schema=DatabaseConstants.FACEBOOK_SCHEMA)
 public class ApplicationUser implements Serializable
 {
 	/**
 	 * <b>Column:</b> APUS_ID <br/><br/>
 	 * The id of the user is an assigned number.
 	 */
+	@Id
+	@Column(name=ApplicationUserConstants.PROPERTY_ID,length=ApplicationUserConstants.LENGTH_ID)
 	private	String	id;
 	
 	public ApplicationUser()
