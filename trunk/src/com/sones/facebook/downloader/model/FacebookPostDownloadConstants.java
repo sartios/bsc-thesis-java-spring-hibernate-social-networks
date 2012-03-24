837de7c31275ab3d92004b1d3e428568e8ac7fef
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-24 11:02:05 +0000

    Incomplete - task : Update facebook downloader model
    Update model. In previous update the table names were not added.

---
+++ trunk/src/com/sones/facebook/downloader/model/FacebookPostDownloadConstants.java
@@ -0,0 +1,14 @@
+package com.sones.facebook.downloader.model;
+
+/**
+ * Contains properties name and lengths for {@link FacebookPostDownload} model.
+ * @author sartios.sones@gmail.com.
+ *
+ */
+public final class FacebookPostDownloadConstants 
+{
+	/**
+	 * The table name.
+	 */
+	public	static	final	String	TABLE_NAME	=	"FACEBOOK_POST_DOWNLOADS";
+}
