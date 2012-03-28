8d1c8dcbde5a0f8fee5fc7e6cecfe6da092fa48a
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-28 08:49:27 +0000

    Incomplete - task : Create keyword searcher logic Add user manager dao and hibernate implementation

---
+++ src/com/sones/userManager/dao/IApplicationUserDao.java
@@ -0,0 +1,9 @@
+package com.sones.userManager.dao;
+
+import com.sones.dao.IGenericDao;
+import com.sones.userManager.model.ApplicationUser;
+
+public interface IApplicationUserDao extends IGenericDao<ApplicationUser, String>
+{
+
+}
