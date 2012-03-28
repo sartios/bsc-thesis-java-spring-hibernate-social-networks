940b9b9c9d0f88cd1511a7c5cc8387f880b39f49
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-28 08:20:00 +0000

    Incomplete - task : Create keyword searcher logic
    Keyword searcher dao and its hibernate implementation

--- src/com/sones/facebook/keywordSearcher/dao/IApplicationUserKeywordDao.java
+++ src/com/sones/facebook/keywordSearcher/dao/IApplicationUserKeywordDao.java
@@ -3,8 +3,15 @@ package com.sones.facebook.keywordSearcher.dao;
 import com.sones.dao.IGenericDao;
 import com.sones.facebook.keywordSearcher.model.ApplicationUserKeyword;
 import com.sones.facebook.keywordSearcher.model.ApplicationUserKeywordId;
+import com.sones.userManager.model.ApplicationUser;
 
 public interface IApplicationUserKeywordDao extends  IGenericDao<ApplicationUserKeyword, ApplicationUserKeywordId>
 {
-
+	/**
+	 * Returns the {@link ApplicationUserKeyword}s for the specified user
+	 * @param appUser
+	 * @return the {@link ApplicationUserKeyword}s for the specified user
+	 * @throws IllegalArgumentException if application user is null.
+	 */
+	public	Iterable<ApplicationUserKeyword>	getByApplicationUser( ApplicationUser appUser );
 }
