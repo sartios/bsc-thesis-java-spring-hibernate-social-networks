a3a7e440309f4f9e42a72185469f5079c3996e45
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-30 09:04:56 +0000

    Change the application's user manager name space

---
+++ trunk/src/com/sones/usermanager/dao/IApplicationUserDao.java
@@ -0,0 +1,9 @@
+package com.sones.usermanager.dao;
+
+import com.sones.dao.IGenericDao;
+import com.sones.usermanager.model.ApplicationUser;
+
+public interface IApplicationUserDao extends IGenericDao<ApplicationUser, String>
+{
+
+}
