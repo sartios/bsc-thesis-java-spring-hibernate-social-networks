027b09017488ce4c523e67c9fcac7501a2c3ad14
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-03-30 09:19:10 +0000

    Change dao to use new app user structure

--- trunk/src/com/sones/facebook/dao/feed/ISourceFacebookPostDao.java
+++ trunk/src/com/sones/facebook/dao/feed/ISourceFacebookPostDao.java
@@ -1,8 +1,11 @@
 package com.sones.facebook.dao.feed;
 
+import java.util.Date;
+
 import com.sones.dao.IGenericDao;
 import com.sones.facebook.model.feed.SourceFacebookPost;
 import com.sones.facebook.model.feed.SourceFacebookPostId;
+import com.sones.facebook.model.source.Source;
 
 /**
  * Provides methods for accessing {@link SourceFacebookPost} class.
@@ -11,5 +14,12 @@ import com.sones.facebook.model.feed.SourceFacebookPostId;
  */
 public interface ISourceFacebookPostDao extends IGenericDao<SourceFacebookPost, SourceFacebookPostId>
 {
-
+	/**
+	 * Returns the source's facebook posts that where downloaded after the specified date.
+	 * @param source for which to retrieve the posts.
+	 * @param date the date that indicates the downloaded date limit.
+	 * @return Source facebook posts.
+	 * @throws	IllegalArgumentException	when source is null or date is null.
+	 */
+	public	Iterable<SourceFacebookPost>	getSourceFacebookPostsBySourceAndAfterDate( Source source, Date date );
 }
