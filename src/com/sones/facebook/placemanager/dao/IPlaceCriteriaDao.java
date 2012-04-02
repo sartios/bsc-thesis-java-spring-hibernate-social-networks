9d3d87535750814ef554f221f499391c64f8d2ea
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-04-02 12:23:41 +0000

    Complete - task : Associate each place that is downloaded with its criterias

---
+++ src/com/sones/facebook/placemanager/dao/IPlaceCriteriaDao.java
@@ -0,0 +1,10 @@
+package com.sones.facebook.placemanager.dao;
+
+import com.sones.dao.IGenericDao;
+import com.sones.facebook.placemanager.model.PlaceCriteria;
+import com.sones.facebook.placemanager.model.PlaceCriteriaId;
+
+public interface IPlaceCriteriaDao	extends	IGenericDao< PlaceCriteria , PlaceCriteriaId >
+{
+
+}
---
+++ src/com/sones/facebook/placemanager/model/PlaceCriteriaConstants.java
@@ -0,0 +1,24 @@
+package com.sones.facebook.placemanager.model;
+
+/**
+ * Contains properties name and length for {@link PlaceCriteria} model.
+ * @author sartios.sones@gmail.com.
+ *
+ */
+public final class PlaceCriteriaConstants 
+{
+	/**
+	 * The table name.
+	 */
+	public	static	final	String	TABLE_NAME	=	"PLACE_CRITERIAS";
+	
+	/**
+	 * The place id property name.
+	 */
+	public	static	final	String	PROPERTY_PLACE	=	"PLCR_PLAC_ID";
+	
+	/**
+	 * The criteria id property name.
+	 */
+	public	static	final	String	PROPERTY_CRITERIA	=	"PLCR_CRIT_ID";
+}
