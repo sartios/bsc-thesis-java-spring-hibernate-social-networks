03986daf8e708b7d5007fb4355153237a361edbf
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-04-02 08:57:26 +0000

    Incomplete - task : Refactor place and checkin Add category and number of checkins into place.

---
+++ trunk/src/com/sones/facebook/model/place/PlaceCategoryConstants.java
@@ -0,0 +1,34 @@
+package com.sones.facebook.model.place;
+
+/**
+ * Contains properties name and lengths of {@link PlaceCategory} model.s
+ * @author sartios.sones@gmail.com.
+ *
+ */
+public final class PlaceCategoryConstants 
+{
+	/**
+	 * The name of the table.
+	 */
+	public	static	final	String	TABLE_NAME	=	"PLACE_CATEGORIES";
+	
+	/**
+	 * The property id name.
+	 */
+	public	static	final	String	PROPERTY_ID	=	"PCAT_ID";
+	
+	/**
+	 * The property description name.
+	 */
+	public	static	final	String	PROPERTY_DESCRIPTION	=	"PCAT_DESCRIPTION";
+	
+	/**
+	 * The length of the id property.
+	 */
+	public	static	final	int	LENGTH_ID	=	20;
+	
+	/**
+	 * The length of the description property.
+	 */
+	public	static	final	int	LENGTH_DESCRIPTION	=	20;
+}
