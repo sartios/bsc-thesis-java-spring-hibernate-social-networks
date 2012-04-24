36c455bbfd445c988e4640eaae649e942faa874f
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-04-24 11:00:45 +0000

    Complete - task : Refactor the IKeywordSearchDao class and HibernateKeywordSearchDao implementation

--- trunk/src/com/sones/facebook/keywordSearcher/dao/IKeywordSearchDao.java
+++ trunk/src/com/sones/facebook/keywordSearcher/dao/IKeywordSearchDao.java
@@ -1,5 +1,8 @@
 package com.sones.facebook.keywordSearcher.dao;
 
+import java.util.Collection;
+import java.util.Date;
+
 import com.sones.dao.IGenericDao;
 import com.sones.facebook.keywordSearcher.model.KeywordSearch;
 import com.sones.usermanager.model.ApplicationUser;
@@ -17,5 +20,15 @@ public interface IKeywordSearchDao extends IGenericDao<KeywordSearch, String>
 	 * @return Keyword search
 	 * @throws IllegalArgumentException if application user is null.
 	 */
-	public	KeywordSearch	getLastKeywordSearchByAppUser( ApplicationUser appUser );
+	public KeywordSearch getLastKeywordSearchByAppUser( ApplicationUser appUser );
+	
+	/**
+	 * Returns the keyword searches that had happened after date and until now
+	 * and belongs to the application user.
+	 * @param date the date after which the searches had happened.
+	 * @param appUser the application user of the searches.
+	 * @return Keyword searches
+	 * @throws IllegalArgumentException if date or application user is null.
+	 */
+	public Collection<KeywordSearch> getAfterDateByAppUser( Date date, ApplicationUser appUser);
 }
--- trunk/test/com/sones/facebook/keywordSearcher/dao/hibernate/HibernateKeywordSearchDaoTester.java
+++ trunk/test/com/sones/facebook/keywordSearcher/dao/hibernate/HibernateKeywordSearchDaoTester.java
@@ -1,12 +1,18 @@
 package com.sones.facebook.keywordSearcher.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sones.dao.IGenericDao;
 import com.sones.facebook.dao.feed.IFacebookPostDao;
 import com.sones.facebook.dao.hibernate.feed.HibernateFacebookPostDao;
 import com.sones.facebook.dao.hibernate.source.HibernateUserDao;
@@ -19,6 +25,7 @@ import com.sones.facebook.keywordSearcher.model.KeywordSearch;
 import com.sones.facebook.model.feed.FacebookPost;
 import com.sones.usermanager.dao.hibernate.HibernateApplicationUserDao;
 import com.sones.usermanager.dao.IApplicationUserDao;
+import com.sones.usermanager.model.ApplicationUser;
 
 import static org.junit.Assert.*;
 
@@ -83,10 +90,98 @@ public class HibernateKeywordSearchDaoTester
 	}
 	
 	@Test
-	public	void	getLastKeywordSearchByAppUser()
+	public void getLastKeywordSearchByAppUser()
 	{
 		KeywordSearch search = keywordSearchDao.getLastKeywordSearchByAppUser( keywordSearch.getUser() );
 		assertNotNull(search);
 		assertEquals("2000-01-03 00:00:00.0", search.getDate().toString());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void getAfterDateByAppUserShouldThrowOnNullDate()
+	{
+		keywordSearchDao.getAfterDateByAppUser(null, new ApplicationUser());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void getAfterDateByAppUserShouldThrowOnNullAppUser()
+	{
+		keywordSearchDao.getAfterDateByAppUser(Calendar.getInstance().getTime(), null);
+	}
+	
+	@Test
+	public void getAfterDateByAppUserShouldWorkWhenAppUserDoesNotExist()
+	{
+		ApplicationUser nonExistentantAppUser = new ApplicationUser();
+		nonExistentantAppUser.setId("aaa");
+		keywordSearchDao.getAfterDateByAppUser(Calendar.getInstance().getTime(),nonExistentantAppUser );
+	}
+	
+	@Test
+	public void getAfterDateByAppUserShouldWorkCorrectly()
+	{
+		ApplicationUser appUser = new ApplicationUser();
+		appUser.setId("15");
+		saveIfDoesNotExist(appUser,appUser.getId(),appUserDao);
+		
+		ApplicationUser appUser2 = new ApplicationUser();
+		appUser2.setId("16");
+		saveIfDoesNotExist(appUser2,appUser2.getId(),appUserDao);
+		
+		List<KeywordSearch> searches = new ArrayList<KeywordSearch>();
+		for(int searchIndex = 0 ; searchIndex < 5; searchIndex++)
+		{
+			KeywordSearch search = new KeywordSearch();
+			search.setId( String.valueOf(searchIndex) );
+			search.setDate( Calendar.getInstance().getTime() );
+			search.setUser(appUser);
+			saveIfDoesNotExist(search,search.getId(),keywordSearchDao);
+			searches.add(search);
+			sleep(1000);
+		}
+		KeywordSearch search = new KeywordSearch();
+		search.setId( String.valueOf(5) );
+		search.setDate( Calendar.getInstance().getTime() );
+		search.setUser(appUser2);
+		saveIfDoesNotExist(search,search.getId(),keywordSearchDao);
+		searches.add(search);
+		
+		Date date = searches.get(0).getDate();
+		Collection<KeywordSearch> results = keywordSearchDao.getAfterDateByAppUser(date, appUser2);
+		
+		assertEquals(1, results.size());
+		
+		for(KeywordSearch keySearch : searches)
+		{
+			deleteIfExists(keySearch, keySearch.getId(), keywordSearchDao);
+		}
+		deleteIfExists(appUser, appUser.getId(), appUserDao);
+		deleteIfExists(appUser2, appUser2.getId(), appUserDao);
+	}
+	
+	private void sleep(long miliseconds)
+	{
+		try {
+			Thread.sleep(miliseconds);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void saveIfDoesNotExist(Object object, Object id, IGenericDao dao)
+	{
+		if(dao.GetById(id) == null)
+		{
+			dao.Save(object);
+		}
+	}
+	
+	private void deleteIfExists(Object object, Object id, IGenericDao dao)
+	{
+		if(dao.GetById(id) != null)
+		{
+			dao.Delete(object);
+		}
 	}
 }
