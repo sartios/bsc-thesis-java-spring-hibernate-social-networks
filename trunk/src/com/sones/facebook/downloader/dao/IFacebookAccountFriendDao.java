9585cb3e1e9c8410874866b70285e50fb38d33a1
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-05-03 14:45:17 +0000

    Add functionality into Facebook account friend dao, for returning by account.

--- trunk/src/com/sones/facebook/downloader/dao/IFacebookAccountFriendDao.java
+++ trunk/src/com/sones/facebook/downloader/dao/IFacebookAccountFriendDao.java
@@ -3,8 +3,11 @@ package com.sones.facebook.downloader.dao;
 import com.sones.dao.IGenericDao;
 import com.sones.facebook.downloader.model.FacebookAccountFriend;
 import com.sones.facebook.downloader.model.FacebookAccountFriendId;
+import com.sones.facebook.tokenmanager.model.FacebookAccount;
 
 public interface IFacebookAccountFriendDao extends IGenericDao<FacebookAccountFriend, FacebookAccountFriendId>
 {
+
+	public Iterable<FacebookAccountFriend> getByOwner(FacebookAccount owner);
 
 }
