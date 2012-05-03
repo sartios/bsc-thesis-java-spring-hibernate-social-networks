18eeb8ad2e7eb713e881d2d6b74a55d89ed96f20
savramis.sartios <savramis.sartios@d6d3ce4e-8a00-fd31-1d6e-3bfeadfa13cc>
2012-05-03 14:45:17 +0000

    Add functionality into Facebook account friend dao, for returning by account.

--- src/com/sones/facebook/downloader/dao/hibernate/HibernateFacebookAccountFriendDao.java
+++ src/com/sones/facebook/downloader/dao/hibernate/HibernateFacebookAccountFriendDao.java
@@ -1,14 +1,37 @@
 package com.sones.facebook.downloader.dao.hibernate;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
 import com.sones.dao.hibernate.HibernateGenericDao;
 import com.sones.facebook.downloader.dao.IFacebookAccountFriendDao;
 import com.sones.facebook.downloader.model.FacebookAccountFriend;
 import com.sones.facebook.downloader.model.FacebookAccountFriendId;
+import com.sones.facebook.tokenmanager.model.FacebookAccount;
 
 public class HibernateFacebookAccountFriendDao extends HibernateGenericDao<FacebookAccountFriend, FacebookAccountFriendId> implements IFacebookAccountFriendDao
 {
 	public HibernateFacebookAccountFriendDao()
 	{
 		super(FacebookAccountFriend.class);
+	}
+
+	@Override
+	public Iterable<FacebookAccountFriend> getByOwner(FacebookAccount owner)
+	{
+
+		Session session = getHibernateTemplate().getSessionFactory().openSession();
+		
+		Criteria criteria = session.createCriteria(FacebookAccountFriend.class)
+			.add( Restrictions.eq("id.owner", owner))
+			.setResultTransformer( Criteria.DISTINCT_ROOT_ENTITY);
+
+		List<FacebookAccountFriend> results = criteria.list();
+		session.close();
+		
+		return results;
 	}
 }
